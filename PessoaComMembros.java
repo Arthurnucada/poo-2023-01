1f5b82db403a72c190cad6e56f70c7e2c2317f5c
Arthurnucada <131211101+Arthurnucada@users.noreply.github.com>
2023-05-08 21:04:13 -0300

    T07

---
+++ PessoaComMembros.java
@@ -0,0 +1,9 @@
+import javax.xml.crypto.Data;
+
+public class PessoaComMembros {
+    private Data nascimento;
+    private String nome;
+    private Pessoa pai;
+    private Pessoa mae;
+
+}
---
+++ ProvaComAtribeMet.java
@@ -0,0 +1,11 @@
+public class ProvaComAtribeMet{
+    prvate byte numQuestoes;
+    public byte getNumQuestoes() {
+        return numQuetoes;
+    }
+    public void setNumquestoes(byte n){
+        if (n > 0) {
+            numQuestoes = n;
+        }
+    }
+}
