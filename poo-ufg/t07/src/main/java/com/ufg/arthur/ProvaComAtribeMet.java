4440e3977501365805b6d8a15cf3d25d014ae009
Arthurnucada <arthurnucada280204@gmail.com>
2023-06-29 11:02:13 -0300

    t07

--- poo-ufg/t07/src/main/java/com/ufg/arthur/ProvaComAtribeMet.java
+++ poo-ufg/t07/src/main/java/com/ufg/arthur/ProvaComAtribeMet.java
@@ -1,15 +1,16 @@
 package com.ufg.arthur;
 
-public class ProvaComAtribeMet{
+public class ProvaComAtribeMet {
     private byte numQuestoes;
+
     public byte getNumQuestoes() {
-        byte numQuetoes;
-        return numQuetoes;
+
+        return numQuestoes;
+        
     }
-    public void setNumquestoes(byte n){
+    public void setNumquestoes(byte n) {
         if (n > 0) {
             numQuestoes = n;
         }
     }
 }
-
--- poo-ufg/t07/src/main/java/com/ufg/arthur/Teste.java
+++ poo-ufg/t07/src/main/java/com/ufg/arthur/Teste.java
@@ -2,6 +2,6 @@ package com.ufg.arthur;
 
 public class Teste {
     public static void main (String[]args){
-        Prova pf - new Prova();
+        Prova pf = new Prova();
     }
 }
