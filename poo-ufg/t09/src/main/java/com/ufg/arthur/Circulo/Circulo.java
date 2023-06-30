0cbdd70c413e80aed72d638524bafca4b1185155
Arthurnucada <arthurnucada280204@gmail.com>
2023-06-30 16:36:47 -0300

    t09

--- poo-ufg/t09/src/main/java/com/ufg/arthur/Circulo/Circulo.java
+++ poo-ufg/t09/src/main/java/com/ufg/arthur/Circulo/Circulo.java
@@ -1,14 +1,6 @@
 package com.ufg.arthur.Circulo;
 
-import java.util.List;
-
 public class Circulo {
     int raio;
     Ponto centro;
-}
-
-void transladar(int dx, int dy){
-    this.x = dx;
-    this.y = dy;
-
 }
