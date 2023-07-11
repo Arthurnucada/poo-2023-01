5920b6b4b163f6ecc6394b74670520e54fbe8a8a
Arthurnucada <arthurnucada280204@gmail.com>
2023-07-10 21:45:33 -0300

    t18

---
+++ poo-ufg/t18/src/main/java/com/ufg/arthur/Peca.java
@@ -0,0 +1,21 @@
+package com.ufg.arthur;
+
+public class Peca {
+    private String nome;
+
+    public Peca(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    public String toString() {
+        return nome;
+    }
+}
